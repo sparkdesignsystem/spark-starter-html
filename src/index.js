49c5ad746463720686d1415f6aa37885165aa9bf
Bob Wise <bobwise@quickenloans.com>
2019-12-18 13:30:31 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "../src/style.scss"
+
+import spark from "@sparkdesignsystem/spark/es5/spark";
+import sparkPrerender from "@sparkdesignsystem/spark/es5/sparkPrerender";
+
+import "@sparkdesignsystem/spark/_spark.scss";
+
+// initialize Spark
+sparkPrerender();
+spark();
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    })
+  ]
+};
