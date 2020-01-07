a920f23b53155d9ef54c0a1ed8183427be9a7860
Bob Wise <bobwise@quickenloans.com>
2020-01-07 14:58:57 -0500

    font setup

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import "../src/style.scss"
 import spark from "@sparkdesignsystem/spark/es5/spark";
 import sparkPrerender from "@sparkdesignsystem/spark/es5/sparkPrerender";
 
-import "@sparkdesignsystem/spark/_spark.scss";
-
 // initialize Spark
 sparkPrerender();
 spark();
