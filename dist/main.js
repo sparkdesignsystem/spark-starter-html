ee3103f3a3af32f27407013640a988a300e9a7dd
Bob Wise <bobwise@quickenloans.com>
2020-01-07 15:00:20 -0500

    build output

--- dist/main.js
+++ dist/main.js
@@ -86,17 +86,6 @@
 /************************************************************************/
 /******/ ({
 
-/***/ "./node_modules/@sparkdesignsystem/spark/_spark.scss":
-/*!***********************************************************!*\
-  !*** ./node_modules/@sparkdesignsystem/spark/_spark.scss ***!
-  \***********************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./node_modules/@sparkdesignsystem/spark/_spark.scss?");
-
-/***/ }),
-
 /***/ "./node_modules/@sparkdesignsystem/spark/es5/spark.js":
 /*!************************************************************!*\
   !*** ./node_modules/@sparkdesignsystem/spark/es5/spark.js ***!
@@ -127,7 +116,7 @@ eval("!function(e,t){if(true)module.exports=t();else { var r, n; }}(window,funct
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _src_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../src/style.scss */ \"./src/style.scss\");\n/* harmony import */ var _src_style_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_src_style_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _sparkdesignsystem_spark_es5_spark__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @sparkdesignsystem/spark/es5/spark */ \"./node_modules/@sparkdesignsystem/spark/es5/spark.js\");\n/* harmony import */ var _sparkdesignsystem_spark_es5_spark__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_sparkdesignsystem_spark_es5_spark__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _sparkdesignsystem_spark_es5_sparkPrerender__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @sparkdesignsystem/spark/es5/sparkPrerender */ \"./node_modules/@sparkdesignsystem/spark/es5/sparkPrerender.js\");\n/* harmony import */ var _sparkdesignsystem_spark_es5_sparkPrerender__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_sparkdesignsystem_spark_es5_sparkPrerender__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _sparkdesignsystem_spark_spark_scss__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @sparkdesignsystem/spark/_spark.scss */ \"./node_modules/@sparkdesignsystem/spark/_spark.scss\");\n/* harmony import */ var _sparkdesignsystem_spark_spark_scss__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_sparkdesignsystem_spark_spark_scss__WEBPACK_IMPORTED_MODULE_3__);\n\n\n\n\n\n\n\n// initialize Spark\n_sparkdesignsystem_spark_es5_sparkPrerender__WEBPACK_IMPORTED_MODULE_2___default()();\n_sparkdesignsystem_spark_es5_spark__WEBPACK_IMPORTED_MODULE_1___default()();\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _src_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../src/style.scss */ \"./src/style.scss\");\n/* harmony import */ var _src_style_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_src_style_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _sparkdesignsystem_spark_es5_spark__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @sparkdesignsystem/spark/es5/spark */ \"./node_modules/@sparkdesignsystem/spark/es5/spark.js\");\n/* harmony import */ var _sparkdesignsystem_spark_es5_spark__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_sparkdesignsystem_spark_es5_spark__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _sparkdesignsystem_spark_es5_sparkPrerender__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @sparkdesignsystem/spark/es5/sparkPrerender */ \"./node_modules/@sparkdesignsystem/spark/es5/sparkPrerender.js\");\n/* harmony import */ var _sparkdesignsystem_spark_es5_sparkPrerender__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_sparkdesignsystem_spark_es5_sparkPrerender__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\n\n// initialize Spark\n_sparkdesignsystem_spark_es5_sparkPrerender__WEBPACK_IMPORTED_MODULE_2___default()();\n_sparkdesignsystem_spark_es5_spark__WEBPACK_IMPORTED_MODULE_1___default()();\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
