ec3b68134eda0433f28d846a8cc5882aa4b73afc
Febbraro <amberfebbraro@quickenloans.com>
2020-06-02 11:11:27 -0400

    add polyfills

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import "../src/style.scss"
+import '../src/style.scss';
 
-import spark from "@sparkdesignsystem/spark/es5/spark";
+import spark from '@sparkdesignsystem/spark/es5/spark';
 
-import sparkPrerender from "@sparkdesignsystem/spark/es5/sparkPrerender";
+import sparkPrerender from '@sparkdesignsystem/spark/es5/sparkPrerender';
 
+import '@sparkdesignsystem/spark/es5/sparkPolyfills';
 
-
-import "./assets/placeholder.jpg"
+import './assets/placeholder.jpg'
 
 // initialize Spark
 sparkPrerender();
