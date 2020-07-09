7b166e7b7169dffdd612fbbb08989d82ae8345db
Bob Wise <bobwise@quickenloans.com>
2020-07-08 22:02:57 -0400

    checkboxes and radios

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import spark from "@sparkdesignsystem/spark/es5/spark";
 
 import sparkPrerender from "@sparkdesignsystem/spark/es5/sparkPrerender";
 
-
-
 import "./assets/placeholder.jpg"
 
 // initialize Spark
