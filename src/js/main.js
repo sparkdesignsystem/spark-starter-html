7c22dffddcb91adc4685dc81fd4e3e12fd8d85b6
Rob Copeland <robcopeland@fastmail.com>
2020-11-04 16:10:26 -0500

    adds hbs process, renaming, cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import '../src/style.scss';
+import '../styles/main.scss';
 
 import spark from '@sparkdesignsystem/spark/es5/spark';
 import sparkPrerender from '@sparkdesignsystem/spark/es5/sparkPrerender';
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,51 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HandlebarsPlugin = require('handlebars-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const fs = require('fs');
+const request = require('request');
+
+function loadIcons(cb) {
+  const fileStream = fs.createWriteStream(
+      "./src/pages/partials/sparkIcons.hbs"
+  );
+  request
+      .get({
+        uri: "https://www.rockomni.com/mcds/assets/GlobalContent/NonStockImages/Icons/spark-icons-v14.svg",
+        rejectUnauthorized: false
+      })
+      .pipe(fileStream)
+      .on("finish", cb);
+}
+
+loadIcons(() => { console.log('done');});
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    js: "./src/js/main.js"
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
   },
   module: {
-    rules: [
-      { test: /\.hbs$/,
-        loader: "handlebars-loader",
-      },
-      {
+    rules: [ {
         test: /\.s[ac]ss$/i,
-
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       }
     ]
   },
 
   plugins: [
-    new HtmlWebpackPlugin({ template: "./public/index.hbs" }),
     new MiniCssExtractPlugin({
       filename: "bundle.css"
+    }),
+    new HandlebarsPlugin({
+      entry: path.join(process.cwd(), "src/pages", "*.html"),
+      output: path.join(process.cwd(), "dist", "index.html"),
+      partials: [
+          path.join(process.cwd(), "src", "pages", "partials", "*.hbs")
+      ]
     })
   ]
 };
