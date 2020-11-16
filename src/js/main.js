788af5ceaccc7b953a35a2ad5cbb23630e17bacc
Rob Copeland <robcopeland@fastmail.com>
2020-11-16 09:51:39 -0500

    adding linting / commit hooks

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ import spark from '@sparkdesignsystem/spark/es5/spark';
 import sparkPrerender from '@sparkdesignsystem/spark/es5/sparkPrerender';
 import '@sparkdesignsystem/spark/es5/sparkPolyfills';
 
-
 // initialize Spark
 sparkPrerender();
 spark();
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,53 @@
-const path = require("path");
+const path = require('path');
 const HandlebarsPlugin = require('handlebars-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const fs = require('fs');
 const request = require('request');
 
-function loadIcons(cb) {
+const loadIcons = (cb) => {
   const fileStream = fs.createWriteStream(
-      "./src/pages/partials/sparkIcons.hbs"
+    './src/pages/partials/sparkIcons.hbs',
   );
   request
-      .get({
-        uri: "https://www.rockomni.com/mcds/assets/GlobalContent/NonStockImages/Icons/spark-icons-v14.svg",
-        rejectUnauthorized: false
-      })
-      .pipe(fileStream)
-      .on("finish", cb);
-}
+    .get({
+      uri:
+        'https://www.rockomni.com/mcds/assets/GlobalContent/NonStockImages/Icons/spark-icons-v14.svg',
+      rejectUnauthorized: false,
+    })
+    .pipe(fileStream)
+    .on('finish', cb);
+};
 
-loadIcons(() => { console.log('done');});
+loadIcons(() => {
+  console.log('done fetching icons');
+});
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    js: "./src/js/main.js"
+    js: './src/js/main.js',
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
-    rules: [ {
+    rules: [
+      {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
 
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "bundle.css"
+      filename: 'bundle.css',
     }),
     new HandlebarsPlugin({
-      entry: path.join(process.cwd(), "src/pages", "*.html"),
-      output: path.join(process.cwd(), "dist", "index.html"),
-      partials: [
-          path.join(process.cwd(), "src", "pages", "partials", "*.hbs")
-      ]
-    })
-  ]
+      entry: path.join(process.cwd(), 'src/pages', '*.html'),
+      output: path.join(process.cwd(), 'dist', 'index.html'),
+      partials: [path.join(process.cwd(), 'src', 'pages', 'partials', '*.hbs')],
+    }),
+  ],
 };
