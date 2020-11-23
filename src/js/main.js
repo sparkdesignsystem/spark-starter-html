dd361d1c87ad3e2087a0c4b21df15283cbf62486
Bob Wise <bobwise@quickenloans.com>
2020-11-23 17:31:08 -0500

    components

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,22 @@ import spark from '@sparkdesignsystem/spark/es5/spark';
 import sparkPrerender from '@sparkdesignsystem/spark/es5/sparkPrerender';
 import '@sparkdesignsystem/spark/es5/sparkPolyfills';
 
+import { setSpinning, cancelSpinning } from '@sparkdesignsystem/spark';
+
 // initialize Spark
 sparkPrerender();
 spark();
+
+window.startSpinners = (classname) => {
+  const spinningButtons = document.querySelectorAll('.' + classname)
+  spinningButtons.forEach((button) => {
+    setSpinning(button, {});
+  })
+}
+
+window.resetSpinners = (classname) => {
+  const spinningButtons = document.querySelectorAll('.sprk-c-Button--has-spinner.' + classname);
+  spinningButtons.forEach((button) => {
+    cancelSpinning(button);
+  })
+}
